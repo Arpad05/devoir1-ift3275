760c120039d62b8ee6f7e067aa2954ac0803b6a8
Arpad Botond Rigo <71524777+Arpad05@users.noreply.github.com>
2024-10-27 23:31:17 -0400

    Create student_code.py

---
+++ student_code.py
@@ -0,0 +1,4 @@
+def decrypt(C):
+  #entrez votre code ici.
+  #Vous pouvez créer des fonctions auxiliaires et adapter le code à votre façon mais decrypt dois renvoyer le message décrypté
+  return M
